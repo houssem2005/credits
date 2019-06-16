d9c61f16a68de46576bf6ce378bae839bfacb7dc
houssem2005 <46058993+houssem2005@users.noreply.github.com>
2019-06-16 15:19:02 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,12 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+setInterval( () => {
+client.channels.get("574520549469061142").send("#daily")
+}, 86400000)
+});
+
+client.login(process.env.BOT_TOKEN);
